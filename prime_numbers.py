c4bcf29b2cdb991afd2baaa220dcb9a089d9efa4
Benyamin <114058200+ARAB-BENYAMIN@users.noreply.github.com>
2022-09-21 14:25:05 +0430

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,14 +1,32 @@
 #prime_numbers in range
-def aval_morabea(num):
+def hvhuin(num):
     flag= True
     for i in range(2,num):
         if num%i==0:
             flag= False
     else:
         return flag
+    
+lower = int(input("write your lower number"))
+upper = int(input("write your upper number"))
 
-
-for i in range (100,1000):
-    test=aval_morabea(i)
+for i in range (lower,upper):
+    test=hvhuin(i)
     if test==True:
         print(i)
+#it is prime number or not
+def check(num):
+    flag= True
+    for i in range(2,num):
+        if num%i==0:
+            flag= False
+    else:
+        return flag
+x= int(input("\t -->"))
+test=check(x)
+if test==True:
+    print("{} is aval".format(x))
+elif x<=1:
+    print("{} is not aval".format(x))
+else:
+    print("{} is not aval".format(x))
